f056822efbcd2957aa68c7637dcad259f5310f76
Bjoern D. Rasmussen <contact@bearware.dk>
2017-03-24 21:46:36 +0100

    Don't exclude features which are somehow still referenced

--- ACE/build.bat
+++ ACE/build.bat
@@ -1,2 +1,3 @@
 set ACE_ROOT=%CD%
-perl %ACE_ROOT%\bin\mwc.pl -type vc10 -static -features ssl=1 -features uses_wchar=1 -features ipv6=1 -features acexml=0 -features ace_svcconf=0 -features ace_token=0 -features ace_idl_dependencies=0 -expand_vars ace protocols
+set SSL_ROOT=C:\OpenSSL
+perl %ACE_ROOT%\bin\mwc.pl -type vc10 -static -features ssl=1 -features uses_wchar=1 -features ipv6=1 -expand_vars ace protocols
