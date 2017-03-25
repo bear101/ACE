fcda5434835df0883c1f96da461b1257916885c3
Bjoern D. Rasmussen <contact@bearware.dk>
2017-03-25 09:05:59 +0100

    Hardcode ACE macros. mpc.pl doesn't pick up ipv6=1

--- ACE/ace/config.h
+++ ACE/ace/config.h
@@ -1,5 +1,17 @@
 #define ACE_DEFAULT_THREAD_KEYS 1088
 #define ACE_USES_IPV4_IPV6_MIGRATION 1
 
+#ifndef ACE_HAS_IPV6
+#define ACE_HAS_IPV6
+#endif
+
+#ifndef ACE_USES_WCHAR
+#define ACE_USES_WCHAR
+#endif
+
+#ifndef ACE_AS_STATIC_LIBS
+#define ACE_AS_STATIC_LIBS
+#endif
+
 #include "ace/config-win32.h"
 
