747f87edb1367ff0b1d088ef777f302db992388b
bear101 <contact@bearware.dk>
2017-08-31 20:19:14 +0200

    Added IPv6 support

--- ACE/ace/config.h
+++ ACE/ace/config.h
@@ -1,2 +1,4 @@
 #include "config-android.h"
-// #define ACE_HAS_IPV6 1
+#define ACE_HAS_IPV6 1
+#define ACE_LACKS_GETIPNODEBYADDR 1 /* missing in android netdb.h */
+#define ACE_LACKS_GETIPNODEBYNAME_IPV6 1
