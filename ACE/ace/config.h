241e07e934aa88d722cfe2e443caae489d45c269
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2018-01-11 08:47:48 +0100

    Generic lib paths

--- ACE/ace/config.h
+++ ACE/ace/config.h
@@ -1,4 +1,5 @@
-#include "config-android.h"
 #define ACE_HAS_IPV6 1
 #define ACE_LACKS_GETIPNODEBYADDR 1 /* missing in android netdb.h */
 #define ACE_LACKS_GETIPNODEBYNAME_IPV6 1
+#define ACE_HAS_CPP11
+#include "config-android.h"
