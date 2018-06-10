fcd701f8704cd14040ad75f16b87c033e1da3a71
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2018-06-10 15:18:16 +0200

    C++11 support and cleaned up previous compile fixes

--- ACE/ace/config.h
+++ ACE/ace/config.h
@@ -1,4 +1,7 @@
 #define ACE_HAS_IPV6 1
+#define ACE_LACKS_GETIPNODEBYADDR 1 /* missing in android netdb.h */
+#define ACE_LACKS_GETIPNODEBYNAME_IPV6 1
 #define ACE_HAS_CPP11
 #include "config-android.h"
+#undef ACE_LACKS_FD_MASK
 
