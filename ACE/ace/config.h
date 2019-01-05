62275c7e4832b6b8ce86394fedc2d16c904d8fe2
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2019-01-05 14:21:21 +0100

    Initial config.h

---
+++ ACE/ace/config.h
@@ -0,0 +1,6 @@
+#define ACE_HAS_IPV6 1
+// #define ACE_LACKS_GETIPNODEBYADDR 1 /* missing in android netdb.h */
+// #define ACE_LACKS_GETIPNODEBYNAME_IPV6 1
+#define ACE_HAS_CPP11
+#include "config-android.h"
+
