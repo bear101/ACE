902bc50c88adbb307d48e72a09cf6d120153d957
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2019-10-31 21:33:56 +0100

    Compile fixes for Xcode 10.3. iPhone simulator 12.4

--- ACE/ace/config.h
+++ ACE/ace/config.h
@@ -1,3 +1,10 @@
 #define ACE_HAS_IPV6 1
 #define ACE_HAS_CPP11
+#define ACE_LACKS_SYSTEM 1 // removed in Xcode 9
 #include "config-macosx-iOS-hardware.h"
+
+// some idiotic defines config-macosx-leopard.h that are incompatible
+// with Xcode 10.3
+#undef ACE_LACKS_CLOCKID_T
+#undef ACE_LACKS_CLOCK_MONOTONIC
+#undef ACE_LACKS_CLOCK_REALTIME
