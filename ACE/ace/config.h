f63a70909c7faefb209296d0851166b2ac17e22b
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2020-03-21 05:38:09 +0100

    Hardcoded ACE configuration for Android NDK r21 with API-level 21

---
+++ ACE/ace/config.h
@@ -0,0 +1,7 @@
+#define ACE_AS_STATIC_LIBS 1
+#define ACE_HAS_IPV6 1
+#define ACE_LACKS_GETIPNODEBYADDR 1 /* missing in android netdb.h */
+#define ACE_LACKS_GETIPNODEBYNAME_IPV6 1
+#define ACE_HAS_CPP11
+#include "config-android.h"
+//#undef ACE_LACKS_FD_MASK
